c88fc5b90d1492fa77eb93560f6a254683c70962
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-07-20 19:57:11 -0500

    added JS into the files, and installed npm

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name of title',
+        message: 'What do you want the name of the title?(Required)',
+        validate: nameInput =>{
+            if(nameInput){
+                return true;
+            }
+            else{
+                console.log('Please enter a title.')
+                return false;
+            };
+        }
+    },
+    {
+        type: 'input',
+        name: 'Purpose',
+        message: 'What is the purpose of the project?(Required)',
+        validate: nameInput =>{
+            if(nameInput){
+                return true;
+            }
+            else{
+                console.log('Please fill out the description')
+                return false;
+            };
+        }
+    },
+    {
+        type: 'input',
+        name: 'Build',
+        message: 'What was it built with?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
