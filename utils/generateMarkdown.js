35991bf91f10269533e026a9d1aa4a31a18c3b67
Lucas Wysoczanski <109931537+LucasWyski001@users.noreply.github.com>
2023-07-23 16:07:25 -0500

    updated JS

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
+
+
 function renderLicenseBadge(license) {
   let badge ='';
   if(license != none){
@@ -14,13 +16,13 @@ function renderLicenseLink(license) {
   let licenseLink;
 
   switch(license){
-    case 'MIT': licenseLink = 'https://mit-license.org/';
+    case 'MIT': licenseLink = '[![MIT License](https://img.shields.io/badge/MIT_LICENSE-blue)](https://opensource.org/licenses/MIT)';
     break;
     case 'BSD': licenseLink = 'https://opensource.org/licenses/BSD-3Clause';
     break;
-    case 'GPL': licenseLink = 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+    case 'GPL': licenseLink = '[![GNU General Public License V3.0](https://img.shields.io/badge/GNU_LICENSE_3.0-blue)](https://opensource.org/license/gpl-3-0/)';
     break;
-    case 'Apache': licenseLink ='https://www.apache.org/licenses/LICENSE-2.0.html';
+    case 'Apache': licenseLink ='[![Apache License 2.0](https://img.shields.io/badge/APACHE_LICENSE_2.0-blue)](https://opensource.org/license/apache-2-0/)';
     break;
     default: licenseLink = 'https://img.shields.io/badge/license-Unlicense-blue.svg';
     break;
@@ -43,7 +45,6 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-  
   ## Description
   ${data.Description}
   
@@ -68,7 +69,7 @@ function generateMarkdown(data) {
   ${data.tests}
   
   ## License
-  This project is licensed under the ${data.licenses} license.
+  This project is licensed under the ${renderLicenseSection(data.license)} license.
   
   ## Questions
   For any questions about the project, please feel free to reach out to [${data.github}](https://github.com/${data.github}) or contact via email at ${data.email}.`;
